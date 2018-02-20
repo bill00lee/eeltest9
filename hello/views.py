096c032a269c02e4641af31a2cdd10b92cbdea22
bill00lee@hotmail.com <bill00lee@gmail.com>
2018-02-20 12:49:14 -0500

    added forms on contact page

--- hello/views.py
+++ hello/views.py
@@ -2,26 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from gettingstarted.forms import ContactForm
+
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     context = {
-        "title":"title i inserted"
+        "title":"homepage",
+        "content":"info about the homepage"
     }
     return render(request, 'index.html', context)
 
 def about_page(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'about_page.html')
+    context = {
+        "title":"about",
+        "content":"info about the about"
+    }
+    return render(request, 'about_page.html', context)
 
 def contact_page(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'contact/views.html')
-
-    if request.method == "POST":
-         print(request.POST)
-         print(request.POST.get('fullname'))
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title":"Contact page",
+        "content":" Welcome to the contact page.",
+        "form": contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
     #     print(request.POST.get('email'))
     #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
