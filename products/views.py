387ec7838fff95de737b44b8675f78d8724de3c4
bill00lee@hotmail.com <bill00lee@gmail.com>
2018-03-07 17:49:47 -0500

    model manager

--- products/views.py
+++ products/views.py
@@ -7,13 +7,16 @@ from .models import Product
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 
 def product_list_view(request):
@@ -38,12 +41,14 @@ class ProductDetailView(DetailView):
 def product_detail_view(request, pk=None, *args, **kwargs):
     #instance = Product.objects.get(pk=pk) #id
     #instance = get_object_or_404(Product, pk=pk)
-    qs  = Product.objects.filter(id=pk)
-    if qs.exists() and qs.count() == 1:
-        instance = qs.first()
-    else:
-        raise Http404("Product doesn't exists")
-
+    #qs  = Product.objects.filter(id=pk)
+    #if qs.exists() and qs.count() == 1:
+    #    instance = qs.first()
+    #else:
+    #    raise Http404("Product doesn't exists")
+    instance = Product.objects.get_by_id(pk)
+    if instance is none:
+        raise Http404("Product does not exist")
 
     context = {
         'object': instance
